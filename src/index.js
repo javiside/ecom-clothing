7498aa22c552f4374cfaf87c0d5cadacc5616978
javiside <javi.mtz@hotmail.com>
2021-02-05 01:28:03 -0700

    created cart-item component and connected cart dropdown with cartItems reducer

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
-import "./styles/index.css";
+import "./styles/index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
